58124476edd9cae0b63cea3ec86a958ae3734e00
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-04-08 09:54:46 +0700

    auto deploy

--- src/component/atom/Container.jsx
+++ src/component/atom/Container.jsx
@@ -6,7 +6,7 @@ const Container = ({title, description, children,id}) => {
 
 
     return (
-        <div id={id} className={`flex flex-col h-screen pt-32 justify-center items-center mt-10 p-10  gap-10`}>
+        <div id={id} className={`flex flex-col md:h-screen pt-32 justify-center items-center mt-10 p-10  gap-10`}>
             <div className={`flex flex-col items-center`}  data-aos="fade-up">
 
                 <Paragraf >{title}</Paragraf>
