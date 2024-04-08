ddd9a7d3035a281f8f47336e9ff48bf2bc8e3b4f
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-04-08 10:01:10 +0700

    auto deploy

--- src/component/organisms/Navbar.jsx
+++ src/component/organisms/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                         <BtnNav />
                     </div>
                 </div>
-                {/* <BtnThemes /> */}
+                <BtnThemes />
             </div>
         </Router>
     )
